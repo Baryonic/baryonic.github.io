0ff86757ca95ef89854edd06252270b0cc632f8c
baryonic <franxumansurfer@gmail.com>
2025-05-26 16:34:01 +0200

    videos!!
    default cards finished until more projects are complete

--- js/script.js
+++ js/script.js
@@ -2,3 +2,16 @@ console.log("Script Loaded Successfully");
 function f1(){
     console.log("Function f1 executed");
 }
+
+/*Video-stop detection */
+document.addEventListener("DOMContentLoaded", function () {
+    const bgVideo = document.querySelector('.bg-video');
+    if (bgVideo) {
+        // Try to keep the video playing if it stops unexpectedly
+        setInterval(() => {
+            if (bgVideo.paused || bgVideo.ended) {
+                bgVideo.play().catch(() => {});
+            }
+        }, 2000); // Check every 2 seconds
+    }
+});
